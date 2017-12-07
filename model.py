abc81588dc216327dfe3ecdca653366ec34c5791
Javier Ventajas Hernandez <javier.ventajas@scout24.ch>
2017-12-07 14:41:30 +0100

    Finish implementation

--- model.py
+++ model.py
@@ -12,6 +12,16 @@ import torchvision.transforms as transforms
 import torchvision.models as models
 
 
+
+use_cuda = torch.cuda.is_available()
+dtype = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
+
+imsize = 512 if use_cuda else 128
+
+content_layers_default = ['conv_4']
+style_layers_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+
+
 class ContentLoss(nn.Module):
 
     def __init__(self, target, weight):
@@ -43,7 +53,7 @@ class StyleLoss(nn.Module):
 
     def __init__(self, target, weight):
         super(StyleLoss, self).__init__()
-        self.target = target.detach * weight
+        self.target = target.detach() * weight
         self.weight = weight
         self.gram = GramMatrix()
         self.criterion = nn.MSELoss()
@@ -56,7 +66,7 @@ class StyleLoss(nn.Module):
         return self.output
 
     def backward(self, retain_graph=True):
-        self.loss.backward(retrain_graph=retain_graph)
+        self.loss.backward(retain_graph=retain_graph)
         return self.loss
 
 
@@ -77,43 +87,8 @@ def show_image(tensor, title=None):
     plt.pause(0.001)
 
 
-use_cuda = torch.cuda.is_available()
-dtype = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
-
-
-imsize = 512 if use_cuda else 128
-
-loader = transforms.Compose([transforms.Scale(imsize), transforms.ToTensor()])
-
-style_img = load_image("").type(dtype)
-content_img = load_image("").type(dtype)
-
-assert style_img.size() == content_img.size(), \
-    "Style and content image must have the same size"
-
-
-unloader = transforms.ToPILImage()
-
-plt.ion()
-
-plt.figure()
-show_image(style_img.data, title='Style Image')
-
-plt.figure()
-show_image(content_img.data, title='Content Image')
-
-cnn = models.vgg19(pretrained=True).features
-
-if use_cuda:
-    cnn = cnn.cuda()
-
-content_layers_default = ['conv_4']
-style_layers_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
-
-
 def get_style_model_and_losses(cnn, style_img, content_img, style_weight=1000, content_weight=1,
                                content_layers=content_layers_default, style_layers=style_layers_default):
-
     cnn = copy.deepcopy(cnn)
 
     content_losses = []
@@ -136,14 +111,106 @@ def get_style_model_and_losses(cnn, style_img, content_img, style_weight=1000, c
                 target = model(content_img).clone()
                 content_loss = ContentLoss(target, content_weight)
                 model.add_module("content_loss_" + str(i), content_loss)
+                content_losses.append(content_loss)
+
+            if name in style_layers:
+                target_feature = model(style_img).clone()
+                target_feature_gram = gram(target_feature)
+                style_loss = StyleLoss(target_feature_gram, style_weight)
+                model.add_module("style_loss_" + str(i), style_loss)
+                style_losses.append(style_loss)
+
+            i += 1
+
+        if isinstance(layer, nn.MaxPool2d):
+            name = "pool_" + str(i)
+            model.add_module(name, layer)
+
+    return model, style_losses, content_losses
 
 
+def get_input_param_optimizer(input_img):
+    input_param = nn.Parameter(input_img.data)
+    optimizer = optim.LBFGS([input_param])
+    return input_param, optimizer
 
 
+def run_style_transfer(cnn, content_img, style_img, input_img, num_steps=300, style_weight=10000, content_weight=1):
+    print("Building the style transfer model...")
+    model, style_losses, content_losses = get_style_model_and_losses(
+        cnn, style_img, content_img, style_weight, content_weight)
+    input_param, optimizer = get_input_param_optimizer(input_img)
+
+    print("Optimizer...")
+    run = [0]
+    while run[0] <= num_steps:
+
+        def closure():
+            input_param.data.clamp_(0, 1)
+
+            optimizer.zero_grad()
+            model(input_param)
+            style_score = 0
+            content_score = 0
+
+            for sl in style_losses:
+                style_score += sl.backward()
+            for cl in content_losses:
+                content_score += cl.backward()
+
+            run[0] += 1
+            if run[0] % 50 == 0:
+                print("run {}:".format(run))
+                print('Style Loss : {:4f} Content Loss: {:4f}'.format(
+                    style_score.data[0], content_score.data[0]))
+                print()
+
+            return style_score + content_score
+
+        optimizer.step(closure)
+
+    input_param.data.clamp_(0, 1)
+
+    return input_param.data
 
 
+loader = transforms.Compose([transforms.Scale(imsize), transforms.ToTensor()])
 
+style_img = load_image("style/picasso.jpg").type(dtype)
+content_img = load_image("content/dancing.jpg").type(dtype)
 
+assert style_img.size() == content_img.size(), \
+    "Style and content image must have the same size"
 
+unloader = transforms.ToPILImage()
 
+plt.ion()
 
+plt.figure()
+show_image(style_img.data, title='Style Image')
+
+plt.figure()
+show_image(content_img.data, title='Content Image')
+
+cnn = models.vgg19(pretrained=True).features
+
+if use_cuda:
+    cnn = cnn.cuda()
+
+input_img = content_img.clone()
+
+plt.figure()
+show_image(input_img.data, title="Input Image")
+
+output = run_style_transfer(cnn, content_img, style_img, input_img, num_steps=1000)
+
+plt.figure()
+show_image(output, title='Output Image')
+
+plt.ioff()
+plt.show()
+
+output = output.cpu()
+output = output.squeeze(dim=0)
+final_image = unloader(output)
+final_image.save('stylized.jpg')
